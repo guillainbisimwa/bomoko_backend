2184c3713a80ba96d3b94804c5c94d921be48946
guillainbisimwa <guillainbisimwa@hotmail.fr>
2019-04-22 07:20:07 +0200

    Add users and groups

--- app.py
+++ app.py
@@ -7,55 +7,114 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-from base import Movies, db
+from base import db, Utilisateurs, Groups
 db.init_app(app)
 app.app_context().push()
 db.create_all()
 
-class Movies_List(Resource):
+class Utilisateur_List(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('director', type=str, required=False, help='Director of the movie')
-    parser.add_argument('genre', type=str, required=False, help='Genre of the movie')
-    parser.add_argument('collection', type=int, required=True, help='Gross collection of the movie')
+    parser.add_argument('nom', type=str, required=True, help='nom du User') 
+    parser.add_argument('postnom', type=str, required=False, help='postnom du User') 
+    parser.add_argument('password', type=str, required=False, help='password du User') 
+    parser.add_argument('adress', type=str, required=False, help='adress du User') 
+    parser.add_argument('is_validate', type=int, required=False, help='is_validate du User') 
+    parser.add_argument('is_chef_group', type=int, required=False, help='is_chef_group du User') 
+    parser.add_argument('id_group', type=str, required=False, help='id_group du User') 
+    parser.add_argument('type', type=int, required=False, help='type du User') 
+    parser.add_argument('sexe', type=int, required=False, help='sexe du User') 
     
-    def get(self, movie):
-        item = Movies.find_by_title(movie)
+    def get(self, user):
+        item = Utilisateurs.find_by_phone(user)
         if item:
             return item.json()
-        return {'Message': 'Movie is not found'}
+        return {'Message': 'User is not found'}
     
-    def post(self, movie):
-        if Movies.find_by_title(movie):
-            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
-        args = Movies_List.parser.parse_args()
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
+    def post(self, user):
+        if Utilisateurs.find_by_phone(user):
+            return {' Message': 'User with the phone {} already exists'.format(user)}
+        args = Utilisateur_List.parser.parse_args()
+        item = Utilisateurs(user, args['nom'], args['postnom'], args['password'], args['adress'], args['is_validate'], args['is_chef_group'],args['id_group'],args['type'],args['sexe'] )
         item.save_to()
         return item.json()
         
-    def put(self, movie):
-        args = Movies_List.parser.parse_args()
-        item = Movies.find_by_title(movie)
+    def put(self, user):
+        args = Utilisateur_List.parser.parse_args()
+        item = Utilisateurs.find_by_phone(user)
         if item:
-            item.collection = args['collection']
+            item.nom = args['nom']
+            item.postnom = args['postnom']
+            item.password = args['password']
+            item.adress = args['adress']
+            item.is_validate = args['is_validate']
+            item.is_chef_group = args['is_chef_group']
+            item.id_group = args['id_group']
+            item.type = args['type']
+            item.sexe = args['sexe']
             item.save_to()
-            return {'Movie': item.json()}
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
+            return {'User': item.json()}
+        item = Utilisateurs(user, args['nom'], args['postnom'], args['password'], args['adress'], args['is_validate'], args['is_chef_group'],args['id_group'],args['type'],args['sexe'])
         item.save_to()
         return item.json()
             
-    def delete(self, movie):
-        item  = Movies.find_by_title(movie)
+    def delete(self, user):
+        item  = Utilisateurs.find_by_phone(user)
         if item:
             item.delete_()
-            return {'Message': '{} has been deleted from records'.format(movie)}
+            return {'Message': '{} has been deleted from records'.format(user)}
         return {'Message': '{} is already not on the list'.format()}
     
-class All_Movies(Resource):
+class All_Utilisateurs(Resource):
     def get(self):
-        return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
+        return {'Utilisateurs ': list(map(lambda x: x.json(), Utilisateurs.query.all()))}
+
+
+class Group_List(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('adress', type=str, required=False, help='Adress of the group')
+    parser.add_argument('type', type=int, required=True, help='Gross collection of the group')
     
-api.add_resource(All_Movies, '/')
-api.add_resource(Movies_List, '/<string:movie>')
+    def get(self, group):
+        item = Groups.find_by_nom(group)
+        if item:
+            return item.json()
+        return {'Message': 'Groups is not found'}
+    
+    def post(self, group):
+        if Groups.find_by_nom(group):
+            return {' Message': 'Groups with the name {} already exists'.format(group)}
+        args = Group_List.parser.parse_args()
+        item = Groups(group, args['adress'], args['type'])
+        item.save_to()
+        return item.json()
+        
+    def put(self, group):
+        args = Group_List.parser.parse_args()
+        item = Groups.find_by_nom(group)
+        if item:
+            item.type = args['type']
+            item.save_to()
+            return {'Groups': item.json()}
+        item = Groups(group, args['adress'], args['type'])
+        item.save_to()
+        return item.json()
+            
+    def delete(self, group):
+        item  = Groups.find_by_nom(group)
+        if item:
+            item.delete_()
+            return {'Message': '{} has been deleted from records'.format(group)}
+        return {'Message': '{} is already not on the list'.format()}
+    
+class All_Groups(Resource):
+    def get(self):
+        return {'Groups': list(map(lambda x: x.json(), Groups.query.all()))}
+
+
+api.add_resource(All_Utilisateurs, '/users/')
+api.add_resource(Utilisateur_List, '/user/<string:user>')
+api.add_resource(All_Groups, '/groups/')
+api.add_resource(Group_List, '/group/<string:group>')
 
 if __name__=='__main__':
     
--- base.py
+++ base.py
@@ -1,25 +1,50 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Movies(db.Model):
+class Utilisateurs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), unique=True, nullable=False)
-    director = db.Column(db.String(30), unique=False, nullable=False)
-    genre = db.Column(db.String(30), unique=False, nullable=False)
-    collection = db.Column(db.Integer, unique=False, nullable=False)
+    phone = db.Column(db.String(30), unique=True, nullable=False)
+    nom = db.Column(db.String(30), unique=False, nullable=False)
+    postnom = db.Column(db.String(30), unique=False, nullable=False)
+    password = db.Column(db.String(30), unique=False, nullable=False)
+    adress = db.Column(db.String(30), unique=False, nullable=False)
+
+    is_validate = db.Column(db.Integer, unique=False, nullable=False)
+    is_chef_group = db.Column(db.Integer, unique=False, nullable=False)
+
+    id_group = db.Column(db.String(30), unique=False, nullable=True)
     
-    def __init__(self, title, director, genre, collection):
-        self.title = title
-        self.director = director
-        self.genre = genre
-        self.collection = collection
+    type = db.Column(db.Integer, unique=False, nullable=False)
+    sexe = db.Column(db.Integer,  unique=False, nullable=False)
+    
+    def __init__(self, phone, nom, postnom, password, adress, is_validate, is_chef_group, id_group, type, sexe):
+        self.phone = phone
+        self.nom = nom
+        self.postnom = postnom
+        self.password = password
+        self.adress = adress
+        self.is_validate = is_validate
+        self.is_chef_group = is_chef_group
+        self.id_group = id_group
+        self.type = type
+        self.sexe = sexe
         
     def json(self):
-        return {'Title': self.title, 'Director': self.director, 'Genre': self.genre, 'Collection': self.collection}
+        return {
+            'nom': self.nom,
+            'postnom': self.postnom,
+            'password': self.password,
+            'adress': self.adress,
+            'is_validate': self.is_validate,
+            'is_chef_group': self.is_chef_group,
+            'id_group': self.id_group,
+            'type': self.type,
+            'sexe': self.sexe
+        }
     
     @classmethod
-    def find_by_title(cls, title):
-        return cls.query.filter_by(title=title).first()
+    def find_by_phone(cls, phone):
+        return cls.query.filter_by(phone=phone).first()
     
     def save_to(self):
         db.session.add(self)
@@ -29,5 +54,29 @@ class Movies(db.Model):
         db.session.delete(self)
         db.session.commit()
         
-    
 
+class Groups(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(30), unique=True, nullable=False)
+    adress = db.Column(db.String(30), unique=False, nullable=False)
+    type = db.Column(db.Integer, unique=False, nullable=False)
+    
+    def __init__(self, nom, adress, type):
+        self.nom = nom
+        self.adress = adress
+        self.type = type
+        
+    def json(self):
+        return {'Nom': self.nom, 'Adress': self.adress, 'Type': self.type}
+    
+    @classmethod
+    def find_by_nom(cls, nom):
+        return cls.query.filter_by(nom=nom).first()
+    
+    def save_to(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete_(self):
+        db.session.delete(self)
+        db.session.commit()
